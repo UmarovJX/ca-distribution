b3fee0d8e05793e7b17f906fa4af89d636b518e9
umarovjx <umjahongir@gmail.com>
2023-03-31 00:02:41 +0500

    sss

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,7 @@ import axios from './axios.js'
 
 function signIn(username, password) {
   axios
-    .post('common/auth/login', { password, username }, { withCredentials: true })
+    .postForm('common/auth/login', { password, username }, { withCredentials: true })
     .then((response) => console.log(response))
 }
 
