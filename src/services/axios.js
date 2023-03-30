f191838946b1d52f0f7a3e9a02a83ef6ccea127a
umarovjx <umjahongir@gmail.com>
2023-03-31 00:01:09 +0500

    ssss

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,7 @@ import { useSettingsStore } from '../stores/settings'
 import { useRouter } from 'vue-router'
 const router = useRouter()
 const instance = axios.create({
-  mode:'same-origin',
+  mode:'cors',
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     Accept: 'application/json',
